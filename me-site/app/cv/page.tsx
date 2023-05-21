0a739d8b75e2b47941ed56c529909b3396fccf1e
Vinicius Vieira <vinipachecov@gmail.com>
2023-05-20 22:28:10 -0300

    move website to next-js and tailwind

---
+++ me-site/app/cv/page.tsx
@@ -0,0 +1,10 @@
+export default function CV() {
+    return (
+        <main className="bg-gray-100">
+            <div className="w-screen h-screen">
+                <embed src="/files/vinicius-vieira-cv.pdf" className="w-full h-full"
+                    type="application/pdf"></embed>
+            </div>
+        </main>
+    )
+}
