d0e9b10b2ed545c37ec8d78f094d4f34ddb3d089
Vinicius Vieira <vinipachecov@gmail.com>
2023-05-20 22:58:38 -0300

    enforce usage of underscore

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Intro = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center">                  
-      <div className="text-center text-gray-600 lg:text-5xl sm:text-4xl md:text-3xl ">Hi, I`m <strong>Vinicius Pacheco Vieira</strong></div>      
+      <div className="text-center text-gray-600 lg:text-5xl sm:text-4xl md:text-3xl ">Hi, I'm <strong>Vinicius Pacheco Vieira</strong></div>      
       <div className="text-center text-gray-600 lg:text-5xl sm:text-4xl md:text-3xl mt-2"> a Software Developer</div>                                      
       <div className="w-full flex flex-row gap-x-8 justify-center mt-8">
         <a href="/files/vinicius-vieira-cv" className="rounded bg-teal-400 p-2" download>Download CV</a>
